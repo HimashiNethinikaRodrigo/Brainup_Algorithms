427ba1f9107e82932f11d9dcdfacfaf42c141f60
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2021-01-31 20:38:38 +0100

    algorithm to identify valid BST

--- src/com/himashi/algorithm/BST/IdentifyBST.java
+++ src/com/himashi/algorithm/BST/IdentifyBST.java
@@ -1,6 +1,24 @@
 package com.himashi.algorithm.BST;
 
+import java.util.LinkedList;
+
 public class IdentifyBST {
+    public static void main(String[] args) {
+        Root rootWithInvalidBST = BTUtil.getBSTInvalid();
+        Root rootWithValidBST = BTUtil.getBST2();
 
+        System.out.println(isValidBST(rootWithInvalidBST, Integer.MIN_VALUE, Integer.MAX_VALUE));
+        System.out.println(isValidBST(rootWithValidBST, Integer.MIN_VALUE, Integer.MAX_VALUE));
 
+    }
+
+    private static boolean isValidBST(Root root, int min, int max) {
+        if (root == null)
+            return true;
+
+        if (root.value < min || root.value > max) return false;
+
+        return isValidBST(root.leftChild, min, root.value)
+                && isValidBST(root.rightChild, root.value, max);
+    }
 }
