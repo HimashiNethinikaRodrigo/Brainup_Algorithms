d3e57c859888981ae70b07ca97d6e8874957cb9e
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2021-02-01 20:47:11 +0100

    minimum coin challenge

--- src/com/himashi/algorithm/coinchange/MinimumCoinChanging.java
+++ src/com/himashi/algorithm/coinchange/MinimumCoinChanging.java
@@ -2,6 +2,21 @@ package com.himashi.algorithm.coinchange;
 
 public class MinimumCoinChanging {
     public static void main(String[] args) {
+        int[] denominations ={1, 5, 6, 9};
+        int amount = 10;
 
+        System.out.println(minimumCoinChangingResult(denominations, amount));
+    }
+
+    private static int minimumCoinChangingResult(int[] denominations, int amount) {
+        int[] resultList = new int[amount+1];
+        resultList[0] =0;
+        for (int coin : denominations) {
+            for (int i = coin; i < amount+1 ; i++) {
+                if (resultList[i] == 0) resultList[i] = 1 + resultList[i-coin];
+                else resultList[i] = Math.min(resultList[i], 1+ resultList[i-coin]);
+            }
+        }
+        return resultList[resultList.length-1];
     }
 }
