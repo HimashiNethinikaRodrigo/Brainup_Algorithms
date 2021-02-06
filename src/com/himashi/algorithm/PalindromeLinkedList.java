a9d69bf7dfe81b11493088bca390cdd2098b6288
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2021-02-06 12:18:41 +0100

    PalindromeLinkedList

---
+++ src/com/himashi/algorithm/PalindromeLinkedList.java
@@ -0,0 +1,46 @@
+package com.himashi.algorithm;
+
+public class PalindromeLinkedList {
+
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) return false;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        slow = reverse(slow);
+        fast = head;
+
+        while (slow != null) {
+            if (slow.val != fast.val)
+                return false;
+
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return true;
+    }
+
+    ListNode reverse(ListNode head) {
+        ListNode reverse = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = reverse;
+            reverse = head;
+            head = next;
+        }
+
+        return reverse;
+    }
+
+    class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x;}
+    }
+}
